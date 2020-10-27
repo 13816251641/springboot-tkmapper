250afccdb4abef93a8fdc341b947e4ef87bc4855
181268 <lujieni1017>
2020-10-27 11:57:57 +0800

    代码提交

--- src/main/java/com/lujieni/tkmapper/mybatis/service/IBaseService.java
+++ src/main/java/com/lujieni/tkmapper/mybatis/service/IBaseService.java
@@ -5,6 +5,10 @@ import com.lujieni.tkmapper.mybatis.service.insert.IInsertService;
 import com.lujieni.tkmapper.mybatis.service.select.ISelectService;
 import com.lujieni.tkmapper.mybatis.service.update.IUpdateService;
 
+/**
+ * 接口可以是多继承的
+ * @param <T>
+ */
 public interface IBaseService<T> extends IDeleteService<T>, IInsertService<T>, IUpdateService<T>, ISelectService<T> {
 
 
