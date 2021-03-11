03644012445add03243e1c430a3b386054b0ab52
181268 <lujieni1017>
2021-03-11 11:06:15 +0800

    代码提交

--- src/main/java/com/lujieni/tkmapper/controller/PersonController.java
+++ src/main/java/com/lujieni/tkmapper/controller/PersonController.java
@@ -3,9 +3,12 @@ package com.lujieni.tkmapper.controller;
 import com.lujieni.tkmapper.common.RequestDTO;
 import com.lujieni.tkmapper.domain.po.PersonPO;
 import com.lujieni.tkmapper.service.IPersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -19,11 +22,18 @@ import java.util.List;
  */
 @RestController
 public class PersonController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private IPersonService iPersonService;
 
+
     @PostMapping("/query-person-by-name")
-    public List<PersonPO> queryPersonByName(@Valid @RequestBody RequestDTO<String> requestDTO){
+    public List<PersonPO> queryPersonByName(@Valid @RequestBody RequestDTO<String> requestDTO, HttpServletRequest request){
+        String apiKey = request.getHeader("ApiKey");
+        String authorization = request.getHeader("Authorization");
+        logger.info("apiKey:{},authorization:{}",apiKey,authorization);
         return iPersonService.queryPersonByName(requestDTO.getData());
     }
 
